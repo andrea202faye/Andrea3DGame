13616f7adbd114f1e229baafaf76736808bab4d8
andrea202faye <andrea202faye@gmail.com>
2025-12-09 10:52:55 +0000

    player movement

--- Assets/Final/Scripts/PlayerMovement.cs
+++ Assets/Final/Scripts/PlayerMovement.cs
@@ -1,31 +1,5 @@
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
-{
-    public float mouseSensitivity = 200f;
-    public Transform playerBody;
-
-    float xRotation = 0f;
-
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked; // Lock cursor to center
-    }
-
-    void Update()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // prevent full head flip
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
-    }
-}
-
-
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
