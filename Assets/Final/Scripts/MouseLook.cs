18fa5c7a02b53afa5ff5f59c36121c7001446ffe
andrea202faye <andrea202faye@gmail.com>
2025-12-11 14:03:17 +0000

    mouse look

--- Assets/Final/Scripts/MouseLook.cs
+++ Assets/Final/Scripts/MouseLook.cs
@@ -17,7 +17,7 @@ public class MouseLook : MonoBehaviour
         
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-        //Debug.Log(mouseX);
+        
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
